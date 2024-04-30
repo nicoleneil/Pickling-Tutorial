5031968e871fa5d21f6ae9dd6b4b646dd5f3b779
nicoleneil <158202481+nicoleneil@users.noreply.github.com>
2024-04-30 19:16:46 -0400

    Delete Pickling-Tutorial-main directory

--- Pickling-Tutorial-main/static/homepage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("learn_button").addEventListener("click", function() {
-    window.location.href = '/learn'; 
-});
-
-document.getElementById("quiz_button").addEventListener("click", function() {
-    window.location.href = '/quiz'; 
-});
--- Pickling-Tutorial-main/templates/README.md
+++
@@ -1,6 +0,0 @@
-# Pickling-Tutorial
-UI Design Spring 2024 Final Project 
-
-Nicole Neil <br />
-Simrin Ponamgi <br />
-Eva Das
